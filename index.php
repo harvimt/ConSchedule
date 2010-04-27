e748d0ed8b3dd7204f155795ddd3b3488674c7cd
Mark Harviston <harvimt@pdx.edu>
2010-04-27 06:19:37 -0700

    unexpanded, removed vim cruft

--- index.php
+++ index.php
@@ -1,29 +1,29 @@
 <?php
 /*
- *      index.php
+ *		index.php
  *
- *      Copyright © 2008 Dylan Enloe <ninina@koneko-hime>
- *      Copyright © 2009, 2010 Drew Fisher <kakudevel@gmail.com>
+ *		Copyright < 2008 Dylan Enloe <ninina@koneko-hime>
+ *		Copyright < 2009, 2010 Drew Fisher <kakudevel@gmail.com>
  *		ALL RIGHTS RESERVED
- *      
- *      This program is free software; you can redistribute it and/or modify
- *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation; either version 2 of the License, or
- *      (at your option) any later version.
- *      
- *      This program is distributed in the hope that it will be useful,
- *      but WITHOUT ANY WARRANTY; without even the implied warranty of
- *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *      GNU General Public License for more details.
- *      
- *      You should have received a copy of the GNU General Public License
- *      along with this program; if not, write to the Free Software
- *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- *      MA 02110-1301, USA.
+ *		
+ *		This program is free software; you can redistribute it and/or modify
+ *		it under the terms of the GNU General Public License as published by
+ *		the Free Software Foundation; either version 2 of the License, or
+ *		(at your option) any later version.
+ *		
+ *		This program is distributed in the hope that it will be useful,
+ *		but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *		GNU General Public License for more details.
+ *		
+ *		You should have received a copy of the GNU General Public License
+ *		along with this program; if not, write to the Free Software
+ *		Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *		MA 02110-1301, USA.
  */
 
 function __autoload($class_name) {
-    require_once $class_name . '.php';
+	require_once $class_name . '.php';
 }
 
 $C = new Connection();
@@ -131,7 +131,7 @@ if( isset($conday) && ! isset($_GET['endTime']) )
 if( ! isset($startDate) && ! isset($endDate) )
 {
 	$startDate = date_create( $conTimes[0]['start'] );
-	$endDate = date_create( $conTimes[$conDayCount-1]['end'] );	
+	$endDate = date_create( $conTimes[$conDayCount-1]['end'] ); 
 }
 
 $schedule = NULL;
